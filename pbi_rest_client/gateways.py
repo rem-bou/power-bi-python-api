1164546702496b37bdb6787eee08345dc08d9f87
rem-bou <bouvier.remi@gmail.com>
2023-02-15 21:11:46 +1030

    Adding missig gateway calls to API

--- pbi_rest_client/gateways.py
+++ pbi_rest_client/gateways.py
@@ -20,6 +20,8 @@ class Gateways:
         self.datasource = None
         self.datasources = {}
         self.datasource_json = {}
+        self.datasources_users = {}
+        self.user_json = {}
         self.server_name = None
         self.database_name = None
         self.connection_details = None
@@ -230,4 +232,139 @@ class Gateways:
             return self.dataset_parameters
         else:
             logging.error("Failed to retrieve workspaces.")
+            self.client.force_raise_http_error(response)
+
+    # https://learn.microsoft.com/en-us/rest/api/power-bi/gateways/get-datasource-users
+    def get_datasource_users(self, gateway_name: str, datasource_name: str) -> List:
+        self.client.check_token_expiration()
+        self.get_datasources(gateway_name)
+        datasource_found = False
+
+        for item in self.datasources: 
+            if item["datasourceName"] == datasource_name: 
+                logging.info("Found datasource with name: " + datasource_name)
+                datasource_found = True
+                self.datasource_json = item
+                break
+        
+        if datasource_found == False:
+            logging.warning("Unable to find datasource with name: " + datasource_name)
+            return
+
+        url = self.client.base_url + "gateways/" + self.gateway_json['id'] + "/datasources/" + self.datasource_json['id'] + "/users"
+        
+        response = requests.get(url, headers = self.client.json_headers)
+
+        if response.status_code == self.client.http_ok_code:
+            logging.info("Successfully retrieved datasources users.")
+            self.datasources_users = response.json()["value"]
+            return self.datasources_users
+        else:
+            logging.error("Failed to retrieve datasources users.")
+            self.client.force_raise_http_error(response)
+    
+    # https://learn.microsoft.com/en-us/rest/api/power-bi/gateways/delete-datasource
+    def delete_gateway_datasource(self, gateway_name: str, datasource_name: str) -> str:
+        self.client.check_token_expiration()
+        self.get_datasources(gateway_name)
+        datasource_found = False
+
+        for item in self.datasources: 
+            if item["datasourceName"] == datasource_name: 
+                logging.info("Found datasource with name: " + datasource_name)
+                datasource_found = True
+                self.datasource_json = item
+                break
+        
+        if datasource_found == False:
+            logging.warning("Unable to find datasource with name: " + datasource_name)
+            return
+
+        url = self.client.base_url + "gateways/" + self.gateway_json['id'] + "/datasources/" + self.datasource_json['id'] 
+        
+        response = requests.delete(url, headers = self.client.json_headers)
+
+        if response.status_code == self.client.http_ok_code:
+            self.datasource = None
+            return logging.info("Successfully deleted datasource: " + datasource_name + " in gateway: " + gateway_name)
+        else:
+            logging.error("Failed to delete datasource: " + datasource_name + " in gateway: " + gateway_name)
+            self.client.force_raise_http_error(response)
+
+    # https://learn.microsoft.com/en-us/rest/api/power-bi/gateways/delete-datasource-user
+    def delete_gateway_datasource_user(self, gateway_name: str, datasource_name: str, email_address: str) -> str:
+        self.client.check_token_expiration()
+        self.get_datasource_users(gateway_name, datasource_name)
+        user_found = False
+
+        for item in self.datasources_users: 
+            if item["emailAddress"] == email_address: 
+                logging.info("Found email address " + email_address + "in datasource " + datasource_name + "." )
+                user_found = True
+                self.user_json = item
+                break
+        
+        if user_found == False:
+            logging.warning("Unable to find email address " + email_address + "in datasource " + datasource_name + ".")
+            return
+
+        url = self.client.base_url + "gateways/" + self.gateway_json['id'] + "/datasources/" + self.datasource_json['id']  + "/users/" + self.user_json['identifier']
+        
+        response = requests.delete(url, headers = self.client.json_headers)
+
+        if response.status_code == self.client.http_ok_code:
+            self.datasource = None
+            return logging.info("Successfully deleted user: " + email_address + " from datasource: " + datasource_name + " in gateway: " + gateway_name)
+        else:
+            logging.error("Failed to delete user: " + email_address + " from datasource: " + datasource_name + " in gateway: " + gateway_name)
+            self.client.force_raise_http_error(response)
+    
+    # https://learn.microsoft.com/en-us/rest/api/power-bi/gateways/add-datasource-user
+    def add_gateway_datasource_user(self, gateway_name: str, datasource_name: str, user_name: str, access_type: str, principal_type: str) -> str:
+        self.client.check_token_expiration()
+        self.get_datasources(gateway_name)
+        datasource_found = False
+
+        for item in self.datasources: 
+            if item["datasourceName"] == datasource_name: 
+                logging.info("Found datasource with name: " + datasource_name)
+                datasource_found = True
+                self.datasource_json = item
+                break
+        
+        if datasource_found == False:
+            logging.warning("Unable to find datasource with name: " + datasource_name)
+            return
+
+        url = self.client.base_url + "gateways/" + self.gateway_json['id'] + "/datasources/" + self.datasource_json['id']  + "/users"
+        
+        error_parameter = ''
+        
+        if principal_type not in ['App', 'Group', 'None', 'User']:
+            error_parameter = 'principal type'
+        elif access_type not in ['None', 'Read', 'ReadOverrideEffectiveIdentity']:
+            error_parameter = 'access type'
+        if error_parameter != '':
+            logging.error(f'Issue with adding permission for {user_name} due to incorrect {error_parameter} to datasource {datasource_name} on gateway {gateway_name}.')
+            return None
+        if principal_type in ['App', 'Group']:
+            payload = {
+                    "identifier": user_name, 
+                    "principalType": principal_type,
+                    "datasourceAccessRight": access_type
+                }
+        elif principal_type in ['User']:
+            payload = {
+                    "emailAddress": user_name, 
+                    "datasourceAccessRight": access_type
+                }
+
+        response = requests.post(url, json = payload, headers = self.client.json_headers)
+
+        if response.status_code == self.client.http_ok_code:
+            logging.info(f"Successfully adding user {user_name} with access {access_type} as {principal_type} to datasource {datasource_name} on gateway {gateway_name}.")
+            self.dataset_permission_updated = True
+            return self.dataset_permission_updated
+        else:
+            logging.error(f"Failed to add user {user_name} to datasource {datasource_name} on gateway {gateway_name}.")
             self.client.force_raise_http_error(response)
