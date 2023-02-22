61f6ac7c37c4eccc141c0a59d9a0bd4904acd86a
rem-bou <bouvier.remi@gmail.com>
2023-02-22 21:47:02 +1030

    reports.py Add My Workspace option

--- pbi_rest_client/dashboards.py
+++ pbi_rest_client/dashboards.py
@@ -8,6 +8,7 @@ from .workspaces import Workspaces
 
 class Dashboards:
     def __init__(self, client):
+        self.classKeyword = 'dashboards'
         self.client = client
         self.workspaces = Workspaces(client)
         self.dashboards = []
@@ -15,9 +16,16 @@ class Dashboards:
     # https://docs.microsoft.com/en-us/rest/api/power-bi/dashboards/get-dashboards-in-group
     def get_dashboards(self, workspace_name: str) -> List:
         self.client.check_token_expiration()
-        self.workspaces.get_workspace_id(workspace_name)
-
-        url = self.client.base_url + "groups/" + self.workspaces.workspace[workspace_name] + "/dashboards"
+        # self.workspaces.get_workspace_id(workspace_name)
+        url_extension = ''
+        if workspace_name:
+            self.workspaces.get_workspace_id(workspace_name)
+            url_extension = "groups/" + self.workspaces.workspace[workspace_name] + '/' 
+        else:
+            workspace_name = 'My Workspace'
+        
+        url = self.client.base_url + url_extension + self.classKeyword
+        # url = self.client.base_url + "groups/" + self.workspaces.workspace[workspace_name] + "/dashboards"
         
         response = requests.get(url, headers = self.client.json_headers)
 
@@ -26,6 +34,6 @@ class Dashboards:
                 self.dashboards.append(item)
             return self.dashboards
         else:
-            logging.error("Failed to retrieve dashboard.")
+            logging.error(f"Failed to retrieve dashboards from workspace: {workspace_name}.")
             self.client.force_raise_http_error(response)
     
